6f36e5bd5b571b07d7fb2da83b295d9096aa29d1
devansh.jaiswal <devansh.jaiswal@stud.tu-darmstadt.de>
2026-04-05 16:07:59 +0200

    Connected prediction pipeline with the flask webapp

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,request, render_template
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from src.Pipeline.predict_pipeline import CustomData,PredictPipeline
 
 
 application = Flask(__name__)
@@ -18,5 +19,17 @@ def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
     else:
-        data = CustomData
-
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+        )
+        pred_df = data.get_data_as_frame()
+        print(pred_df)
+        predict_pipeline = PredictPipeline()
+        results = predict_pipeline.predict(pred_df)
+        return render_template('home.html',results=results[0])
--- src/Pipeline/predict_pipeline.py
+++ src/Pipeline/predict_pipeline.py
@@ -10,10 +10,17 @@ class PredictPipeline:
         pass    
     
     def predict(self,features):
-        model_path = 'artifacts\model.pkl'
-        preprocessor = 'artifacts\preprocessor.pkl'
-        model = load_object(file_path = model_path)
+        try:
+            model_path = 'artifacts\model.pkl'
+            preprocessor_path = 'artifacts\preprocessor.pkl'
+            model = load_object(file_path = model_path)
+            preprocessor= load_object(file_path = preprocessor_path)
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+            return preds
         
+        except Exception as e:
+            raise CustomException(e,sys)
     
 class CustomData:
     def __init__(self,gender: str,race_ethnicity: str,parental_level_of_education,lunch: str,test_preparation_course: str,reading_score:int,writing_score:int):
